0be09c108455548ad927f4085ebb6a4b676d21f4
kyungmo <hellu13@naver.com>
2019-02-13 20:44:41 +0900

    add login and logout

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -1,17 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blog.views.home, name="home"),
-    path('blog/<int:blog_id>', blog.views.detail, name="detail"),
-    path('blog/new/', blog.views.new, name="new"),
-    path('blog/create', blog.views.create, name="create"),
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
-    path('blog/<int:blog_id>/delete', blog.views.delete, name="delete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
