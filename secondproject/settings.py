15192cd64b74b835aee9191007a618b6a8fd7bd3
kyungmo <hellu13@naver.com>
2019-02-27 19:17:28 +0900

    lasttry

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '&5zm4d2+9$!^=z^a)g-ir(yq+g@84guhax(+0wdvtzz#9t6t@0'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&5zm4d2+9$!^=z^a)g-ir(yq+g@84guhax(+0wdvtzz#9t6t@0')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
